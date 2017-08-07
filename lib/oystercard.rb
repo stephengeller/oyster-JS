eb1740c32aca0708c9c96107dec84aea8459738e
Jenny Bacon <jenniferbacon01@gmail.com>
2017-08-07 15:21:36 +0100

    step6

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,16 @@
 class Oystercard
-
   attr_reader :balance
 
   DEFAULT_BALANCE = 0
-  def initialize(balance = DEFAULT_BALANCE)
+  DEFAULT_MAX_BALANCE = 90
+  def initialize(balance = DEFAULT_BALANCE, max_balance = DEFAULT_MAX_BALANCE)
     @balance = balance
+    @max_balance = max_balance
   end
 
   def top_up(credit)
+    raise "unable to top up as balance would exceed maximum of £#{@max_balance}" if (@balance + credit) > @max_balance
     @balance += credit
   end
-
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,16 +1,25 @@
 require 'oystercard'
 
 describe Oystercard do
-
   subject { described_class.new }
 
-  it "is initialized with a balance of 0" do
+  it 'is initialized with a balance of 0' do
     expect(subject.balance).to eq(0)
   end
 
-  it "balance has been topped up with 5" do
+  it 'balance has been topped up with 5' do
     subject.top_up(5)
     expect(subject.balance).to eq 5
+  end
+
+  it 'raises an error when you try to top your balance to above a maximum balance of 90' do
+    expect { subject.top_up(100) }.to raise_error 'unable to top up as balance would exceed maximum of £90'
+  end
+
+  it 'balance has been reduced by 5' do
+    subject.top_up(20)
+    subject.deduct(5)
+    expect(subject.balance).to eq 15
   end
 
 end
