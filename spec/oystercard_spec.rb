58882b3ca76c388c56c9580ef3312819bc7f6647
Olivia Frost <olivia.frost617@googlemail.com>
2017-08-07 13:06:56 +0100

    Step 4 complete, Oystercard class initialized with default balance of 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,11 @@
 require 'oystercard'
 
 describe Oystercard do
+
+  subject { described_class.new }
+
+  it "is initialized with a balance of 0" do
+    expect(subject.balance).to eq(0)
+  end
+    
 end
